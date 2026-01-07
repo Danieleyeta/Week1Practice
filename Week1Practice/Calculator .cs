e054f0fb156936572696935627008c4267f54ef9
DanielEyeta <DanielEyeta@DESKTOP-QJUPI5R>
2026-01-07 15:35:22 +0100

    Add project files.

---
+++ Week1Practice/Calculator .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Practice
+{
+    internal class Calculator
+    {
+        public static void Run()
+        {
+
+            Console.WriteLine("Input first number");
+            string input1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(input1);
+
+            Console.WriteLine("input second number");
+            string input2 = Console.ReadLine();
+            int num2 = Convert.ToInt32(input2);
+
+            int addition = num1 + num2;
+            Console.WriteLine("the addition is" + addition);
+
+        }
+    }
+}
---
+++ Week1Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Week1Practice; // <--- THIS LINE FIXES THE CALCULATOR ERROR
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Choose a program to run:");
+        Console.WriteLine("1. Calculator");
+        Console.WriteLine("2. Temperature Converter (Coming Soon)");
+
+        string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            Calculator.Run();
+        }
+        else if (choice == "2")
+        {
+            // TempConverter.Run(); // <--- COMMENTED OUT UNTIL YOU CREATE THE FILE
+            Console.WriteLine("You haven't built this yet!");
+        }
+    }
+}
